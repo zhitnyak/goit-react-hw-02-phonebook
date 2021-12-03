0cfb720fe3d740bae6f45359aae55742c3a3ea81
Zhitnyak Maksim <m.zhitnyak@gmail.com>
2021-12-03 18:40:44 +0200

    add fn del button

--- src/App.js
+++ src/App.js
@@ -1,30 +1,57 @@
 import React, { Component } from "react";
 import "./App.css";
 import { nanoid } from "nanoid";
-import Form from "./components/Forma";
+import Form from "./components/Forma/Forma";
+import Filter from "./components/Filter/Filter";
+import ContactList from "./components/ContactList/ContactList";
 // import Vidget from "./Vidget";
 // import TodoList from "./TodoList/TodoList";
 // import initialTodos from "./TodoList/todos.json";
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ],
     filter: "",
   };
 
   formSubmitHandler = (data) => {
     console.log(data);
   };
+
+  changeFilter = (e) => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getVisibleContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase(normalizedFilter)
+    );
+  };
+
+  deleteContact = (contactId) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter(
+        (contact) => contact.id !== contactId
+      ),
+    }));
+  };
   render() {
+    const { filter } = this.state;
+    const visibleContacts = this.getVisibleContacts();
     return (
       <>
         <h1>Phonebook</h1>
         <Form onSubmit={this.formSubmitHandler}> </Form>
         <h2>Contacts</h2>
-        <label>
-          Find contacts by name
-          <input type="text"></input>
-        </label>
+        <Filter value={filter} onChange={this.changeFilter} />
+        <ContactList contacts={visibleContacts} onChange={this.deleteContact} />
       </>
     );
   }
---
+++ src/components/ContactList/ContactList.js
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+import css from "./ContactList.module.css";
+
+const ContactList = ({ contacts, onChange }) => {
+  return (
+    <ul className={css.list}>
+      {contacts.map(({ id, name, number }) => (
+        <li className={css.item} key={id}>
+          {`${name}: ${number}`}
+          <button
+            className={css.btn}
+            type="button"
+            onClick={() => onChange(id)}
+          >
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+ContactList.propTypes = {
+  contacts: PropTypes.object,
+  onChange: PropTypes.func,
+};
+export default ContactList;
